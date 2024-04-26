da5896d757df72fe63191fda90a717b007365f29
misiasty <igormisiek1906@gmail.com>
2024-04-26 02:58:15 +0200

    Added implementation of creating cards and the deck. Added pulling card functionality

--- deck.hpp
+++ deck.hpp
@@ -1,6 +1,11 @@
 #include <vector>
+#include <algorithm> 
+#include <random> 
 
-enum card_suits {
+std::random_device rd; 
+std::mt19937 twister(rd()); 
+
+enum card_suit {
     CLUBS, SPADES, HEARTS, DIAMONDS
 };
 
@@ -9,46 +14,145 @@ enum card_colour {
 };
 
 enum card_rank {
-    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
 };
+
+int value_from_rank(card_rank rank){
+    if (rank >= TWO && rank <= TEN) {
+        return rank + 2;
+    } else if (rank >= JACK && rank <= KING) {
+        return 10;
+    } else if (rank == ACE) {
+        return 11;
+    } else {
+        return -1;
+    }
+}
 
 class card
 {
 private:
-    const static card_suits suit;
-    const static card_colour colour;
-    const static card_rank rank;
+    card_suit suit;
+    card_colour colour;
+    card_rank rank;
+    int value;
     
 public:
-    card(/* args */);
-    ~card();
+    card(const card_rank rank, const card_suit suit){
+        this->rank = rank;
+        this->suit = suit;
+        this->value = value_from_rank(rank);
+
+        if(suit == CLUBS || suit == SPADES){
+            this->colour = BLACK;
+        }
+        else{
+            this->colour = RED;
+        }
+    }
+
+    ~card() {}
+
+    card_suit get_suit(){
+        return this->suit;
+    }
+
+    card_colour get_colour(){
+        return this->colour;
+    }
+
+    card_rank get_rank(){
+        return this->rank;
+    }
+
+    int get_value(){
+        return this->value;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const card& c) {
+        os << "Rank: ";
+        switch (c.rank) {
+            case TWO: os << "Two"; break;
+            case THREE: os << "Three"; break;
+            case FOUR: os << "Four"; break;
+            case FIVE: os << "Five"; break;
+            case SIX: os << "Six"; break;
+            case SEVEN: os << "Seven"; break;
+            case EIGHT: os << "Eight"; break;
+            case NINE: os << "Nine"; break;
+            case TEN: os << "Ten"; break;
+            case JACK: os << "Jack"; break;
+            case QUEEN: os << "Queen"; break;
+            case KING: os << "King"; break;
+            case ACE: os << "Ace"; break;
+            default: os << "Unknown"; break;
+        }
+        os << ", Suit: ";
+        switch (c.suit) {
+            case CLUBS: os << "Clubs"; break;
+            case SPADES: os << "Spades"; break;
+            case HEARTS: os << "Hearts"; break;
+            case DIAMONDS: os << "Diamonds"; break;
+            default: os << "Unknown"; break;
+        }
+        os << ", Color: ";
+        switch (c.colour) {
+            case BLACK: os << "Black"; break;
+            case RED: os << "Red"; break;
+            default: os << "Unknown"; break;
+        }
+        os << ", Value: " << c.value;
+        return os;
+    }
 };
 
-card::card(/* args */)
-{
-}
-
-card::~card()
-{
-}
 
 class deck
 {
 private:
-    const static int starting_size;
-    static int current_size;
+    int starting_size;
+    int current_size;
 
     std::vector<card> cards;
 
 public:
-    deck(/* args */);
-    ~deck();
+    deck() {}
+    ~deck() {}
+
+    int get_starting(){
+        return this->starting_size;
+    }
+
+    int get_size(){
+        return this->current_size;
+    }
+
+    void prepare_deck(){
+        std::vector<card> deck;
+
+        for (int suit = CLUBS; suit <= DIAMONDS; ++suit) {
+            for (int rank = TWO; rank <= ACE; ++rank) {
+                deck.push_back(card(static_cast<card_rank>(rank), static_cast<card_suit>(suit)));
+            }
+
+            shuffle(deck.begin(), deck.end(), twister); 
+
+            this->starting_size = deck.size();
+            this->current_size = this->starting_size;
+
+            this->cards = deck;
+        }
+    }
+
+    card pull_card(){
+        std::uniform_int_distribution<int> range(1, current_size);
+        int rand_int = range(twister);
+
+        card pulled_card = cards[rand_int];
+
+        cards.erase(cards.begin() + rand_int);
+        current_size -= 1;
+
+        return pulled_card;
+    }
 };
-
-deck::deck(/* args */)
-{
-}
-
-deck::~deck()
-{
-}
--- game_runner.cpp
+++ game_runner.cpp
@@ -1,6 +1,19 @@
+#include<iostream>
+
 #include "deck.hpp"
 
 int main(int argc, char const *argv[])
 {
+    deck card_deck;
+    card_deck.prepare_deck();
+
+    std::cout << "Starting deck size is: " << card_deck.get_starting() << std::endl;
+
+    for(int i = 0; i < 5; i++){
+        card pulled = card_deck.pull_card();
+        std::cout << "Pulled card is: " << pulled << std::endl;
+        std::cout << card_deck.get_size() << " cards remaining in deck" << std::endl;
+    }
+
     return 0;
 }
