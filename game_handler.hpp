787c9a2e082918dc0f48911e812d0bebba1f2046
misiasty <igormisiek1906@gmail.com>
2024-05-06 04:12:03 +0200

    Main idea behind game handler

--- game_handler.hpp
+++ game_handler.hpp
@@ -1,1 +1,24 @@
 #include "deck.hpp"
+
+class game_handler
+{
+private:
+    std::vector<int> player_points;
+
+public:
+    game_handler() {}
+
+    ~game_handler() {}
+
+    void add_players(std::vector<std::vector<card>>& hands){
+        int hand_value;
+
+        for(std::vector<card>& hand : hands) {
+            // Get starting hands and initialise starting game state
+        }
+    }
+
+    void update_player(std::vector<card> hand, int position){
+        // From hand update game state for player at given position
+    }
+};
